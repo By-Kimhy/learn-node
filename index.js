3d8f5a54ddf3f09f29ac6a867c119c5ea0075921
ByKimhy <kimhyby99@gmail.com>
2026-07-04 21:32:10 +0700

    array, object, array object

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
     data(){
         return{
-            txt:"Hello VueJS"
+            txt:"Hello VueJS",
+
+            colorLish:[
+                "Green","Yellow","Grey"
+            ],
+            obj:{
+                "id":1,"name":"HTML",
+            }
         }
     },
     methods:{
