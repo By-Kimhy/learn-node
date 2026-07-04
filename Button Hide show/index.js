7d2b292db781ff7e51a695acbe69eda2f5377637
Kim hy <kimhyby99@gmail.com>
2026-07-05 00:40:05 +0700

    is active and img hover

---
+++ Button Hide show/index.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            isShow: true,
+            itemList:[
+                "red","green","blue","yellow"
+            ],
+            objlist:[
+                {id:1,name:"HTML",img:"img/1.png",img2:"img/2.png",img1:"img/1.png",isActive:true},
+                {id:2,name:"CSS",img:"img/2.png",img2:"img/3.png",img1:"img/2.png",isActive:false},
+                {id:3,name:"JS",img:"img/3.png",img2:"img/1.png",img1:"img/3.png",isActive:true},
+            ],
+        }
+    },
+    methods:{
+        newImg(i){
+            this.objlist[i].img = this.objlist[i].img2;
+        },
+
+        oldImg(i){
+            this.objlist[i].img = this.objlist[i].img1;
+        },
+        hideShow(){
+            this.isShow = !this.isShow;
+        },
+    },
+});
+app.mount('#app');
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const app = Vue.createApp({
     data(){
         return{
-            txt:"Hello VueJS",
+            txt:"b",
 
             colorLish:[
                 "Green","Yellow","Grey"
             ],
             obj:{
                 "id":1,"name":"HTML",
-            }
+            },
+            price:30,
+            itemList:[
+                {"id":1,"name":"HTML"},
+                {"id":2,"name":"CSS"},
+                {"id":3,"name":"JS"},
+            ],
+            isActive:true,
         }
     },
     methods:{
         setText(){
-            this.txt="Hello Settext"
-        }
+            this.txt="Hello Settext";
+        },
+        setVal(){
+            this.price=100;
+        },
 
     }
 });
