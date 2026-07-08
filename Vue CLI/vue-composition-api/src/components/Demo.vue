9667d6abc95faaa53363e48478bad0260e2b9898
ByKimhy <kimhyby99@gmail.com>
2026-07-08 11:06:02 +0700

    composition API

---
+++ Vue CLI/vue-composition-api/src/components/Demo.vue
@@ -0,0 +1,48 @@
+<script setup>
+    import { ref } from 'vue';
+    const txt = ref("JavaScript");
+    const isShow = ref(true);
+    const isActive = ref(false);
+    function DemoShow() {
+        isShow.value = !isShow.value
+    };
+    const itemList = ref(["HTML", "CSS", "JS"]);
+    const SetText = () => {
+        txt.value = "VueJS3";
+        console.log(txt)
+    }
+    const setActive=()=>{
+        isActive.value=!isActive.value;
+    }
+</script>
+
+<template>
+    <div>
+        <button @click="SetText">SetText</button>
+        <button @click="DemoShow">Hide/Show</button>
+        <button @click="setActive">Set CSS</button>
+        <div class="demo" v-if="isShow" :class="{ 'active': isActive }">
+            {{ txt }}
+        </div>
+        <ul>
+            <li v-for="(item, i) in itemList" :key="i">{{ item }}</li>
+        </ul>
+    </div>
+
+</template>
+
+
+<style scoped>
+    .demo {
+        width: 600px;
+        height: 300px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 60px;
+        background-color: aqua;
+    }
+    .active{
+        background-color: pink;
+    }
+</style>
