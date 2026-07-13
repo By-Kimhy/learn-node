ec482e05c3e1fe78dd10e3bcf12e2b72e8b3e05f
Kim hy <kimhyby99@gmail.com>
2026-07-13 09:47:57 +0700

    create watch component page

--- Vue CLI/vue-watchWebsite/src/App.vue
+++ Vue CLI/vue-watchWebsite/src/App.vue
@@ -1,40 +1,8 @@
+<script setup >
+  import ImgList from './components/ImgList.vue'
+
+</script>
+
 <template>
-  <div class="container">
-    <div class="item-box">
-      1
-    </div>
-    <div class="item-box">
-      1
-    </div>
-    <div class="item-box">
-      1
-    </div>
-    <div class="item-box">
-      1
-    </div>
-
-  </div>
+  <ImgList />
 </template>
-
-<style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  background-color: blue;
-}
-.container {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-wrap: wrap;
-  background-color: black;
-}
-
-.item-box {
-  flex: 1 1 24.5%; /* each takes half width */
-  height: 50%;   /* each takes half height */
-  background-color: aqua;
-}
-
-</style>
---
+++ Vue CLI/vue-watchWebsite/src/components/ImgList.vue
@@ -0,0 +1,104 @@
+<script setup>
+    import { ref } from 'vue';
+    const itemList = ref(
+        [
+            {
+                'title': 'Zinvo Jumbo VD Blaze',
+                'price': 500,
+                'img': '/src/assets/img/1-1.webp',
+                'img1': '/src/assets/img/1-1.webp',
+                'img2': '/src/assets/img/1-2.webp',
+            },
+            {
+                'title': 'Zinvo Jumbo VD Rose Gold',
+                'price': 500,
+                'img': '/src/assets/img/2-1.webp',
+                'img1': '/src/assets/img/2-1.webp',
+                'img2': '/src/assets/img/2-2.webp',
+            },
+            {
+                'title': 'D1 MILANO UTBL38 Chocolush Ultra Thin 30 mm',
+                'price': 395,
+                'img': '/src/assets/img/3-1.webp',
+                'img1': '/src/assets/img/3-1.webp',
+                'img2': '/src/assets/img/3-2.webp',
+            },
+            {
+                'title': 'Zinvo Blade 12K Amber Gem',
+                'price': 420,
+                'img': '/src/assets/img/4-1.webp',
+                'img1': '/src/assets/img/4-1.webp',
+                'img2': '/src/assets/img/4-2.webp',
+            }
+        ]
+    );
+    const newImg = (item) => {
+        item.img=item.img2
+    }
+    const oldImg=(item)=>{
+        item.img=item.img1
+    }
+</script>
+
+<template>
+    <div class="container">
+        <div class="item-box" v-for="(item, index) in itemList" :key="index">
+            <div class="img-box" @mouseover="newImg(item)" @mouseleave="oldImg(item)">
+                <img :src="item.img">
+            </div>
+            <div class="txt-box">
+                <h1>{{item.title}}</h1>
+                <p>$ {{item.price}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scope>
+    .container {
+        width: 81%;
+        margin: auto;
+        background-color: whitesmoke;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .item-box {
+        width: 24.5%;
+        height: 200px;
+    }
+
+    .item-box .img-box {
+        width: 100%;
+        height: 70%;
+    }
+
+    .item-box .img-box img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+
+    .item-box .txt-box {
+        width: 100%;
+        height: 30%;
+    }
+
+    .item-box .txt-box h1 {
+        font-size: 8px;
+        text-align: center;
+        color: #000;
+    }
+
+    .item-box .txt-box p {
+        font-size: 10px;
+        text-align: center;
+        color: #000;
+    }
+</style>
