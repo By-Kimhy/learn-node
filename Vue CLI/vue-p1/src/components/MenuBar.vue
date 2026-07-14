6794acf771c63352178b50f591b0859ea7a81007
ByKimhy <kimhyby99@gmail.com>
2026-07-14 17:55:54 +0700

    create new P1

---
+++ Vue CLI/vue-p1/src/components/MenuBar.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+    <div class="menu-bar">
+        <div class="menu-bar-item">
+            <ul class="menu-bar-item-title">
+                <li class="menu-bar-item-title-text">
+                    
+                    <i class="fa-brands fa-searchengin" ></i>
+                    <span class="menu-bar-item-title-text-icon-text">Home</span>
+                </li>
+                <li class="menu-bar-item-title-text">
+                    <i class="fa-solid fa-user"></i>
+                    <span class="menu-bar-item-title-text-icon-text">User</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
