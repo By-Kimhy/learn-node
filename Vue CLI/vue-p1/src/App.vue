5725376ac7efc61d1752022ddafb303f31e66267
ByKimhy <kimhyby99@gmail.com>
2026-07-16 16:13:27 +0700

    create Friend List

--- Vue CLI/vue-p1/src/App.vue
+++ Vue CLI/vue-p1/src/App.vue
@@ -4,6 +4,7 @@
   import Popup from './components/Popup.vue'
   import HeroSection from './components/HeroSection.vue'
   import CityList from './components/CityList.vue'
+  import FriendList from './components/FriendList.vue'
   import { isDrawer } from './components/store'
 </script>
 <template>
@@ -12,4 +13,5 @@
   <HeroSection />
   <Popup v-if="isDrawer" @click="isDrawer = false" />
   <CityList />
+  <FriendList />
 </template>
---
+++ Vue CLI/vue-p1/src/components/FriendList.vue
@@ -0,0 +1,72 @@
+<script setup>
+    import { ref } from 'vue';
+    import { itemList } from './store'
+    var hideShow = ref(false);
+    const HideShowFriendBox = () => {
+        hideShow.value = !hideShow.value;
+    }
+</script>
+
+<template>
+    <div class="friend-box" :class="{'hide':hideShow}">
+        <h1 @click="HideShowFriendBox">Friend List</h1>
+        <ul>
+            <li v-for="(item,i) in itemList" :key="i">
+                <img :src="item.img" alt="">
+                <span>{{item.name}}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+<style scoped>
+.friend-box{
+    position: fixed;
+    right: 9px;
+    bottom: 9px;
+    width: 300px;
+    height: 400px;
+    background-color: white;
+    box-shadow: 1px 1px 6px dimgrey;
+}
+.friend-box h1{
+    font-size: 15px;
+    height: 39px;
+    background-color: #eee;
+    border-bottom: 1px solid #ccc;
+    display: flex;
+    align-items: center;
+    padding-left: 6px;
+    cursor: pointer;
+}
+.friend-box h1:hover{
+    background-color: #ccc;
+}
+.friend-box ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.friend-box ul li{
+    display: flex;
+    align-items: center;
+    padding: 5px;
+    gap: 10px;
+}
+.friend-box ul li:hover{
+    cursor: pointer;
+    background-color: #ccc;
+}
+.friend-box ul li img{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 1px solid #ccc;
+    
+}
+.friend-box ul li span{
+    font-size: 12px;
+}
+.hide{
+    height: 39px;
+}
+</style>
