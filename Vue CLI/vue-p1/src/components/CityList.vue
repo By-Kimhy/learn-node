12c0d42f87cc6b2ca3fe9a5d8521d174d0eb4b01
ByKimhy <kimhyby99@gmail.com>
2026-07-16 14:17:13 +0700

    update component city vue

---
+++ Vue CLI/vue-p1/src/components/CityList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-4 col-xl-4 col-lg-4">
+                <select class="form-control"></select>
+                <select class="form-control"></select>
+                <select class="form-control"></select>
+            </div>
+        </div>
+    </div>
+</template>
--- Vue CLI/vue-p1/src/components/store.js
+++ Vue CLI/vue-p1/src/components/store.js
@@ -1,4 +1,5 @@
-import { ref } from 'vue'
+import { ref } from 'vue';
+export const isDrawer = ref(false);
 export const itemList = ref([
     {
         id: 1,
@@ -30,6 +31,60 @@ export const itemList = ref([
         ],
         isShow:false
     }
-])
+]);
 
-export const isDrawer = ref(false)
+export const cityList = ref([
+    {
+        name: 'Phnom Penh',
+        sub: [
+            {
+                name: 'TK',
+                sub: [
+                    'TK1',
+                    'TK2',
+                    'TK3'
+                ]
+            },{
+                name:'BKK',
+                sub:[
+                    'BKK1',
+                    'BKK2',
+                    'BKK3'
+                ]
+            },{
+                name:'TTP',
+                sub:[
+                    'TTP1',
+                    'TTP2',
+                    'TTP3'
+                ]
+            }
+        ],
+    },{
+        name: 'Siem Reap',
+        sub: [
+            {
+                name: 'SR1',
+                sub: [
+                    'SR1-1',
+                    'SR1-2',
+                    'SR1-3'
+                ]
+            },{
+                name:'SR2',
+                sub:[
+                    'SR2-1',
+                    'SR2-2',
+                    'SR2-3'
+                ]
+            },{
+                name:'SR3',
+                sub:[
+                    'SR3-1',
+                    'SR3-2',
+                    'SR3-3'
+                ]
+            }
+        ],
+    }
+]);
