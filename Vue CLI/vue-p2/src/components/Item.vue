8ba5a9095d2de4d91d4b85a2b4e89e699a54cf5c
ByKimhy <kimhyby99@gmail.com>
2026-07-17 15:35:20 +0700

    fetch API,asys, await with loading

--- Vue CLI/vue-p2/src/components/Item.vue
+++ Vue CLI/vue-p2/src/components/Item.vue
@@ -1,16 +1,18 @@
 <script setup>
-import { watchEffect, computed } from 'vue';
+import { watchEffect, onMounted, computed, ref } from 'vue';
 import { itemList } from './store.js'
 import { useRouter } from 'vue-router';
 const router = useRouter();
-
 const props = defineProps(['id']);
-// watchEffect used to watch for changes in props console
+const data = ref([]);
+const isLoading = ref(true);
+
+//watchEffect used to watch for changes in props console
 
 // watchEffect(() => {
-//      console.log(props.id);
-//      console.log(itemList.value);
-//  });
+//     console.log(props.id);
+//     console.log(itemList.value);
+// });
 
 const getMenuName = computed(() => {
     return itemList.value.filter(e => e.id == props.id);
@@ -19,14 +21,37 @@ const getMenuName = computed(() => {
 const backHome = () => {
     router.push('/');
 }
+
+const fetchApi = async () => {
+    isLoading.value = true;
+    let url = 'https://reanweb.com/api/teaching/get-menu.php';
+    let rp = await fetch(url);
+    let rs = await rp.json();
+    data.value = rs;
+    console.log(data.value);
+    isLoading.value = false;
+}
+
+onMounted(() => {
+    fetchApi();
+})
+
 </script>
 
 <template>
     <div>
-        <h1>Item {{ $route.params.id }}</h1>
+        <!-- <h1>Item {{ $route.params.id }}</h1>
         <h2>Item Props {{ props.id }}</h2>
         <h2>Menu Item {{ getMenuName[0].name }}</h2>
-        <button @click="backHome">Back Home</button>
+        <button @click="backHome">Back Home</button> -->
+        <div>
+            <h1>Fetch API</h1>
+            <h1 v-if="isLoading">loading...</h1>
+            <h2 v-else v-for="(d,i) in data" :key="i">
+                {{ d.name }}
+            </h2>
+        </div>
     </div>
+
 
 </template>
